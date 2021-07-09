428b6e1e9f4c61e20cce726c73fa1feb0f64a640
Hans Seller <67608613+HanOlo@users.noreply.github.com>
2021-07-08 23:26:41 -0400

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -2,7 +2,7 @@
 
 #data and libraries were loaded with load.R script
 #Tidy data set with date_time variable is edc_df_2 from plot2.R
- 
+
 
 #creates png
 png("plot4.png")
@@ -11,17 +11,17 @@ png("plot4.png")
 par(mfcol = c(2,2))
 
 # Plot 2 (location 1,1)
-plot(edc_df_2$d_time,
-     edc_df_2$Global_active_power,
+plot(edc_df$d_time,
+     edc_df$Global_active_power,
      type = "l",
      xlab = "",
      ylab = "Gloabl Active Power (kilowatts)")
 
 #plot 3 (location 2,1)
-plot(edc_df_2$d_time, edc_df_2$Sub_metering_1,
+plot(edc_df$d_time, edc_df$Sub_metering_1,
      type = "l", xlab = "", ylab = "Energy sub metering")
-lines(edc_df_2$d_time, edc_df_2$Sub_metering_2, col = "red")
-lines(edc_df_2$d_time, edc_df_2$Sub_metering_3, col = "blue")
+lines(edc_df$d_time, edc_df$Sub_metering_2, col = "red")
+lines(edc_df$d_time, edc_df$Sub_metering_3, col = "blue")
 
 #creates legend
 legend("topright", 
@@ -30,16 +30,16 @@ legend("topright",
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" )) 
 
 #plot voltage over datetime (location 1,2)
-plot(edc_df_2$d_time,
-     edc_df_2$Voltage,
+plot(edc_df$d_time,
+     edc_df$Voltage,
      type = "l",
      xlab = "datetime",
      ylab = "Voltage")
 
 
 #plot reactive_power over datetime (location 2,2)
-plot(edc_df_2$d_time,
-     edc_df_2$Global_reactive_power,
+plot(edc_df$d_time,
+     edc_df$Global_reactive_power,
      type = "l",
      xlab = "datetime",
      ylab = "Global_reactive_power")
